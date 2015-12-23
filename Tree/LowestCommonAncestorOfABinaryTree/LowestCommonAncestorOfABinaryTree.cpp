0431342a869fec2211c9cf51ca2ea2337f91ad8b
yijingbai <everatnight@gmail.com>
2015-12-22 22:31:09 -0600

    finished LCA of a binary tree

--- Tree/LowestCommonAncestorOfABinaryTree/LowestCommonAncestorOfABinaryTree.cpp
+++ Tree/LowestCommonAncestorOfABinaryTree/LowestCommonAncestorOfABinaryTree.cpp
@@ -2,15 +2,15 @@
 // Author : Yijing Bai
 // Date   : 2015-12-22
 
-/********************************************************************************** 
- * 
- * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the 
+/**********************************************************************************
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the
  * tree.
- * 
- * According to the definition of LCA on Wikipedia: “The lowest common ancestor is 
- * defined between two nodes v and w as the lowest node in T that has both v and w as 
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is
+ * defined between two nodes v and w as the lowest node in T that has both v and w as
  * descendants (where we allow a node to be a descendant of itself).”
- * 
+ *
  *         _______3______
  *        /              \
  *     ___5__          ___1__
@@ -18,18 +18,62 @@
  *    6      _2       0       8
  *          /  \
  *          7   4
- * 
- * For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example 
- * is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according 
+ *
+ * For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example
+ * is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according
  * to the LCA definition.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> path_p, path_q;
+
+        if (root == NULL) {
+            return NULL;
+        }
+
+        get_path(root, p, path_p);
+        get_path(root, q, path_q);
+
+        TreeNode* result = NULL;
+        for (int i = 0, j = 0; i < path_p.size() && j < path_q.size(); i++, j++) {
+            if (path_p[i] == path_q[j]) {
+                result = path_p[i];
+                continue;
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+
+    bool get_path(TreeNode* root, TreeNode* n, vector<TreeNode*>& path) {
+        if (root == NULL) {
+            return true;
+        }
+
+        path.push_back(root);
+        if (root == n) {
+            return true;
+        }
+        if (root->right != NULL) {
+            if(get_path(root->right, n, path)) return true;
+        }
+        if (root->left != NULL) {
+            if(get_path(root->left, n, path)) return true;
+        }
+
+        path.pop_back();
+        return false;
+    }
+};
 
 
--- Tree/LowestCommonAncestorOfABinaryTree/README.md
+++ Tree/LowestCommonAncestorOfABinaryTree/README.md
@@ -1,4 +1,37 @@
 #[LowestCommonAncestorOfABinaryTree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `236`
+######Tag: `Tree`
+######Difficulty: `Medium`
+
+LCA is a problem that could be difficult. However, for this problem, we just need to use the most naive solution.
+First, we need to store the path from the root node to the target node. So we need a helper funtion to do this.
+This funtion is easy, but easy to be wrong.
+
+```cpp
+    bool get_path(TreeNode* root, TreeNode* n, vector<TreeNode*>& path) {
+        if (root == NULL) {
+            return true;
+        }
+
+        path.push_back(root);
+        if (root == n) {
+            return true;
+        }
+        if (root->right != NULL) {
+            if(get_path(root->right, n, path)) return true;
+        }
+        if (root->left != NULL) {
+            if(get_path(root->left, n, path)) return true;
+        }
+
+        path.pop_back();
+        return false;
+    }
+```
+Initially, the function do not have a return value, which cause OOM. The reason is that after find the path, the funtion will
+keep recursively traverse all the node with out stop. Only the base case returned and two recursion call not returned.
+To fix this bug, we add a bool to indicate whether we already found a path. If so, directly return the path. The reason
+why we store the path in the parameter is that if we keep the variable in the function, every time, we need to merge the value
+with the vector hold by itself. To avoid this, we put the path in parameter, such that the global path is only one.
+
+We could improve this by find the two path in the single traverse.
