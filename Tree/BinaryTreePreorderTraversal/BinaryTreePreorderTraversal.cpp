e24ed3be1e11a294389f62bd0b765c27228ab1ec
yijingbai <everatnight@gmail.com>
2015-12-31 13:16:45 -0600

    Added binary tree preorder traversal

---
+++ Tree/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/binary-tree-preorder-traversal/
+// Author : Yijing Bai
+// Date   : 2015-12-31
+
+/********************************************************************************** 
+ * 
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * 
+ * return [1,2,3].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ Tree/BinaryTreePreorderTraversal/README.md
@@ -0,0 +1,4 @@
+#[BinaryTreePreorderTraversal](https://leetcode.com/problems/binary-tree-preorder-traversal/)
+######No: ``
+######Tag: ``
+######Difficulty: ``
