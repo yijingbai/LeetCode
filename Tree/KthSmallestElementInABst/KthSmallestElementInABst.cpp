ac557f31a831466d4aaedba158b41690014dcc5c
yijingbai <everatnight@gmail.com>
2015-12-31 15:59:04 -0600

    finished Kth smallest element in a BST

--- Tree/KthSmallestElementInABst/KthSmallestElementInABst.cpp
+++ Tree/KthSmallestElementInABst/KthSmallestElementInABst.cpp
@@ -2,31 +2,57 @@
 // Author : Yijing Bai
 // Date   : 2015-12-31
 
-/********************************************************************************** 
- * 
- * Given a binary search tree, write a function kthSmallest to find the kth smallest 
+/**********************************************************************************
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest
  * element in it.
- * 
- * Note: 
+ *
+ * Note:
  * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
- * 
+ *
  * Follow up:
- * What if the BST is modified (insert/delete operations) often and you need to find 
+ * What if the BST is modified (insert/delete operations) often and you need to find
  * the kth smallest frequently? How would you optimize the kthSmallest routine?
- * 
+ *
  *   Try to utilize the property of a BST.
  *   What if you could modify the BST node's structure?
  *   The optimal runtime complexity is O(height of BST).
- * 
+ *
  * Credits:Special thanks to @ts for adding this problem and creating all test cases.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int count = countNodes(root->left);
+        if (k <= count) {
+            return kthSmallest(root->left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root->right, k - 1 -count);
+        }
 
+        return root->val;
+    }
+
+    int countNodes(TreeNode* node) {
+        if (!node) return 0;
+        return countNodes(node->left) + 1 + countNodes(node->right);
+    }
+};
--- Tree/KthSmallestElementInABst/README.md
+++ Tree/KthSmallestElementInABst/README.md
@@ -1,4 +1,11 @@
 #[KthSmallestElementInABst](https://leetcode.com/problems/kth-smallest-element-in-a-bst/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `230`
+######Tag: `Tree`
+######Difficulty: `Medium`
+We can solve this using binary search with the property of the Binary Search Tree.
+Since we need to find the kth smallest, we need to count the number of nodes. It can be done
+easily using a recursion. Then given a root node, we know all the node in the left side of the
+node is less than the root. We just need to count the number of node at the left side. Then judge 
+whether the count number is smaller or bigger than the k. If k smaller than the count, we know the
+kth smallest element will be in the left side. Otherwise, it will be the root or the right side.
+Remember that the root itself also count the node.
