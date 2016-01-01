d7cc4c30bbc0f23e033d397de44d8fbbd3fd8644
yijingbai <everatnight@gmail.com>
2016-01-01 17:35:57 -0600

    Added construct binary tree from preorder traversal

---
+++ Tree/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+// Author : Yijing Bai
+// Date   : 2016-01-01
+
+/********************************************************************************** 
+ * 
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ * 
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ Tree/ConstructBinaryTreeFromPreorderAndInorderTraversal/README.md
@@ -0,0 +1,4 @@
+#[ConstructBinaryTreeFromPreorderAndInorderTraversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)
+######No: ``
+######Tag: ``
+######Difficulty: ``
