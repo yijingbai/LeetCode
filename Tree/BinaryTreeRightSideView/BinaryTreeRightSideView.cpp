a64dbd8002644024448d107e29d2ed060ff376bc
yijingbai <everatnight@gmail.com>
2015-12-31 19:09:03 -0600

    finished binary tree right side view

--- Tree/BinaryTreeRightSideView/BinaryTreeRightSideView.cpp
+++ Tree/BinaryTreeRightSideView/BinaryTreeRightSideView.cpp
@@ -2,28 +2,56 @@
 // Author : Yijing Bai
 // Date   : 2015-12-31
 
-/********************************************************************************** 
- * 
- * Given a binary tree, imagine yourself standing on the right side of it, return the 
+/**********************************************************************************
+ *
+ * Given a binary tree, imagine yourself standing on the right side of it, return the
  * values of the nodes you can see ordered from top to bottom.
- * 
+ *
  * For example:
  * Given the following binary tree,
- * 
- *    1            
- * 
+ *
+ *    1
+ *
  * You should return [1, 3, 4].
- * 
- * Credits:Special thanks to @amrsaqr for adding this problem and creating all test 
+ *
+ * Credits:Special thanks to @amrsaqr for adding this problem and creating all test
  * cases.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result, 0);
+        return result;
+    }
 
+    void dfs(TreeNode* root, vector<int>& result, int depth) {
+        if (!root) return;
+
+        if (depth == result.size()) {
+            result.push_back(root->val);
+        }
+
+        dfs(root->right, result, depth + 1);
+        dfs(root->left, result, depth + 1);
+    }
+
+};
 
--- Tree/BinaryTreeRightSideView/README.md
+++ Tree/BinaryTreeRightSideView/README.md
@@ -1,4 +1,11 @@
 #[BinaryTreeRightSideView](https://leetcode.com/problems/binary-tree-right-side-view/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `199`
+######Tag: `Tree`
+######Difficulty: `Medium`
+Quoting from #[here](https://leetcode.com/discuss/31348/my-simple-accepted-solution-java), the key idea is:
+1. Each depth of the tree only select one node.
+2. View depth is current size of result list.
+Therefore, we can add a judgement that when current depth equals to the size of the result, we can add the current
+node into the result. Otherwise, if the current depth is smaller than the size of the result, which means there is
+already a node in the result, we just recursive find the next level of the tree. To do this, we just need to recursive
+call on the right node and the left node. Noticing that the right node is called first.
