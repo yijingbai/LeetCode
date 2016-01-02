3371f307823f89b1043f076dd39a3f49ab5559dd
yijingbai <everatnight@gmail.com>
2016-01-01 23:04:00 -0600

    added construct tree from inorder and post order traversal

---
+++ Tree/ConstructBinaryTreeFromInorderAndPostorderTraversal/ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+// Author : Yijing Bai
+// Date   : 2016-01-01
+
+/********************************************************************************** 
+ * 
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ * 
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ Tree/ConstructBinaryTreeFromInorderAndPostorderTraversal/README.md
@@ -0,0 +1,4 @@
+#[ConstructBinaryTreeFromInorderAndPostorderTraversal](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)
+######No: ``
+######Tag: ``
+######Difficulty: ``
