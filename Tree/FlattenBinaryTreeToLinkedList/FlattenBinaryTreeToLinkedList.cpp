d127b6cb7f93e96f93bcc45bc2def0edd0d18cb4
yijingbai <everatnight@gmail.com>
2016-01-02 13:02:18 -0600

    flatten binary tree

--- Tree/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.cpp
+++ Tree/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.cpp
@@ -2,21 +2,21 @@
 // Author : Yijing Bai
 // Date   : 2016-01-02
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a binary tree, flatten it to a linked list in-place.
- * 
+ *
  * For example,
  * Given
- * 
+ *
  *          1
  *         / \
  *        2   5
  *       / \   \
  *      3   4   6
- * 
+ *
  * The flattened tree should look like:
- * 
+ *
  *    1
  *     \
  *      2
@@ -28,18 +28,69 @@
  *            5
  *             \
  *              6
- * 
+ *
  * click to show hints.
- * 
+ *
  * Hints:
- * If you notice carefully in the flattened tree, each node's right child points to the 
+ * If you notice carefully in the flattened tree, each node's right child points to the
  * next node of a pre-order traversal.
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
+#include <iostream>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        TreeNode* dummyHead = new TreeNode(-1);
+		TreeNode* currentHead = dummyHead;
+        dfs(root, currentHead);
+        *root = *dummyHead->right;
+    }
+
+	TreeNode* dfs(TreeNode* root, TreeNode* dummyHead) {
+        if (!dummyHead) return dummyHead;
+        if (!root) return dummyHead;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        root->left = NULL;
+        root->right = NULL;
+        dummyHead->right = root;
+        TreeNode* last = dfs(left, dummyHead->right);
+        TreeNode* finally = dfs(right, last);
+        return finally;
+    }
+
+};
+
+int main() {
+	TreeNode* root = new TreeNode(1);
+	TreeNode* n1 = new TreeNode(2);
+	TreeNode* n2 = new TreeNode(3);
+	TreeNode* n3 = new TreeNode(4);
+	TreeNode* n4 = new TreeNode(5);
 
 
+	n2->left = n4;
+	n1->left = n2;
+	n1->right = n3;
+	root->left = n1;
 
+	Solution s;
+	s.flatten(root);
+	cout << "hello" << endl;
+}
--- Tree/FlattenBinaryTreeToLinkedList/README.md
+++ Tree/FlattenBinaryTreeToLinkedList/README.md
@@ -1,4 +1,13 @@
 #[FlattenBinaryTreeToLinkedList](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `114`
+######Tag: `Tree`
+######Difficulty: `Medium`
+The idea for this problem is not hard, however, the implementation can be tricky, since we need to manipulate
+the node inplace. The idea is to DFS the tree and make a new dummy head and add the root to the dummy head.
+It is pretty easy, but we need to remember that the parameter is passed by value and if we want change it,
+it self will not changed unless we changed the content it points to. So when we use recursion, and change the
+dummy head, it will not changed. To fix this, we need to return the changed pointer in the end and use it as
+the new dummy head. 
+
+So If we face the problem like inplace manipulate, we'd better use iterative approach instead of the recursion
+approach.
