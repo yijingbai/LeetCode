87f5f38b0c1fd2b5567247aef687f5a56f358c56
yijingbai <everatnight@gmail.com>
2016-01-01 23:56:30 -0600

    finished sum root to leaf number

--- Tree/SumRootToLeafNumbers/README.md
+++ Tree/SumRootToLeafNumbers/README.md
@@ -1,4 +1,9 @@
 #[SumRootToLeafNumbers](https://leetcode.com/problems/sum-root-to-leaf-numbers/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `129`
+######Tag: `Tree`
+######Difficulty: `Medium`
+This question is just depth first search but we need to get all the path from the root to the leaf node.
+To do this, we need a helper funtion to generate this and then we can calulate the result.
+There is a problem that initionally I just want to add the path in to the path_set when the root is empty,
+however, it will double the result since every node has two empty child node. Then I change to when the node
+is leaf, then add the path into the pathset, this also obey the defination.
--- Tree/SumRootToLeafNumbers/SumRootToLeafNumbers.cpp
+++ Tree/SumRootToLeafNumbers/SumRootToLeafNumbers.cpp
@@ -2,33 +2,100 @@
 // Author : Yijing Bai
 // Date   : 2016-01-01
 
-/********************************************************************************** 
- * 
- * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could 
+/**********************************************************************************
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could
  * represent a number.
  * An example is the root-to-leaf path 1->2->3 which represents the number 123.
- * 
+ *
  * Find the total sum of all root-to-leaf numbers.
- * 
+ *
  * For example,
- * 
+ *
  *     1
  *    / \
  *   2   3
- * 
+ *
  * The root-to-leaf path 1->2 represents the number 12.
  * The root-to-leaf path 1->3 represents the number 13.
- * 
+ *
  * Return the sum = 12 + 13 = 25.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    i
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void sumNumbersHelper(TreeNode* root, vector<vector<int>>& route_set, vector<int>& route) {
+        if (!root) {
+            return;
+        }
+        route.push_back(root->val);
+        if (!root->left && !root->right) {
+            route_set.push_back(route);
+        }
+        sumNumbersHelper(root->left, route_set, route);
+        sumNumbersHelper(root->right, route_set, route);
+        route.pop_back();
+    }
+
+    int sumNumbers(TreeNode* root) {
+        vector<vector<int>> result;
+        vector<int> route;
+        sumNumbersHelper(root, result, route);
+        int total_sum = 0;
+        for (int i = 0; i < result.size(); i++) {
+            int sum = 0;
+            int prefix = 1;
+            for (int j = result[i].size() - 1; j >= 0; j--) {
+                sum += result[i][j] * prefix;
+                prefix *= 10;
+            }
+            total_sum += sum;
+        }
+        return total_sum;
+    }
+};
 
 
 
+int main() {
+    TreeNode* root = new TreeNode(2);
+    TreeNode* n1 = new TreeNode(0);
+    TreeNode* n2 = new TreeNode(0);
+    root->left = n1;
+    root->right = n2;
+
+    Solution s;
+    s.sumNumbers(root);
+}
