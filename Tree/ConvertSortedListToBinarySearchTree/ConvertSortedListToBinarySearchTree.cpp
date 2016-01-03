14f1a427fa081cc91d426a13559b9361c9df78bb
yijingbai <everatnight@gmail.com>
2016-01-02 19:42:41 -0600

    Fihsed convert sorted list to binary search tree

--- Tree/ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree.cpp
+++ Tree/ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTree.cpp
@@ -2,18 +2,69 @@
 // Author : Yijing Bai
 // Date   : 2016-01-02
 
-/********************************************************************************** 
- * 
- * Given a singly linked list where elements are sorted in ascending order, convert it 
+/**********************************************************************************
+ *
+ * Given a singly linked list where elements are sorted in ascending order, convert it
  * to a height balanced BST.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
+#include <iostream>
 
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+	TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return NULL;
 
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = NULL;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
 
+        TreeNode* root = new TreeNode(slow->val);
+        ListNode* rightHead = slow->next;
+
+        if (prev) {
+            prev->next = NULL;
+        } else {
+            head = NULL;
+        }
+
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(rightHead);
+        return root;
+    }
+
+};
+
+int main() {
+	ListNode* head = new ListNode(3);
+	ListNode* n1 = new ListNode(5);
+	ListNode* n2 = new ListNode(8);
+
+	n1->next = n2;
+	head->next = n1;
+
+	Solution s;
+	s.sortedListToBST(head);
+}
--- Tree/ConvertSortedListToBinarySearchTree/README.md
+++ Tree/ConvertSortedListToBinarySearchTree/README.md
@@ -1,4 +1,10 @@
 #[ConvertSortedListToBinarySearchTree](https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `108`
+######Tag: `Tree`
+######Difficulty: `Medium`
+This problem is trivial. Since we want to construct a balanced binary search tree, so we should pick the median as
+the root. Then we recursively pick the left root and right root and set them as the left child and the right child.
+The problem is that when we find the median, we need to split the first half. 
+First, I just want set the median to NULL. However, it does not work again. So I put the find median function into
+the main function and add a prev to record the position before the median. Then we just set the `prev->next = NULL`
+Then we done.
