4646a590cefe985975919b0e3c0ecbcf98b66f07
yijingbai <everatnight@gmail.com>
2016-01-03 16:20:09 -0600

    Finsihed Number of ISland

--- Tree/NumberOfIslands/NumberOfIslands.cpp
+++ Tree/NumberOfIslands/NumberOfIslands.cpp
@@ -2,30 +2,72 @@
 // Author : Yijing Bai
 // Date   : 2016-01-03
 
-/********************************************************************************** 
- * 
- * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An 
- * island is surrounded by water and is formed by connecting adjacent lands 
- * horizontally or vertically. You may assume all four edges of the grid are all 
+/**********************************************************************************
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An
+ * island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are all
  * surrounded by water.
- * 
+ *
  * Example 1:
  * 11110110101100000000
  * Answer: 1
  * Example 2:
  * 11000110000010000011
  * Answer: 3
- * 
- * Credits:Special thanks to @mithmatt for adding this problem and creating all test 
+ *
+ * Credits:Special thanks to @mithmatt for adding this problem and creating all test
  * cases.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
+#include <vector>
 
+using namespace std;
 
+class Solution {
+public:
+    int numIslands(vector<vector<char> >& grid) {
+        int num = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    numIslandsHelper(grid, i, j);
+                    ++num;
+                }
+            }
+        }
+        return num;
+    }
 
+    void numIslandsHelper(vector<vector<char> >& grid,
+        int x, int y) {
+        if (grid[x][y] == '0' || grid[x][y] == 'x') {
+            return;
+        }
+        grid[x][y] = 'x';
+        if (x - 1 >= 0) {
+            numIslandsHelper(grid, x - 1, y);
+        }
+        if (y + 1 < grid[0].size()) {
+            numIslandsHelper(grid, x, y + 1);
+        }
+        if (x + 1 < grid.size()) {
+            numIslandsHelper(grid, x + 1, y);
+        }
+        if (y - 1 >= 0) {
+            numIslandsHelper(grid, x, y - 1);
+        }
+    }
+};
+
+int main() {
+	Solution s;
+	vector<char> in;
+	in.push_back('1');
+	in.push_back('1');
+	vector<vector<char> > input;
+	input.push_back(in);
+
+	s.numIslands(input);
+}
--- Tree/NumberOfIslands/README.md
+++ Tree/NumberOfIslands/README.md
@@ -1,4 +1,8 @@
 #[NumberOfIslands](https://leetcode.com/problems/number-of-islands/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `200`
+######Tag: `DFS BFS`
+######Difficulty: `Medium`
+This is a typical DFS or BFS problem. Both BFS and DFS could solve this problem.
+The idea is that for each entry of the input, if it is 1, we start from it and do DFS or BFS.
+For all the entry that are visited, we set it to another char, like 'x' and we do not visit them
+again. And we traverse each entry that the value is 1. Each time increase the count. Then we done.
