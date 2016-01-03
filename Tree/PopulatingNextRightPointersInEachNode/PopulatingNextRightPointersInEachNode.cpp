d97898ec38914b38c4da9e4a63ce92331173ed16
yijingbai <everatnight@gmail.com>
2016-01-02 20:22:29 -0600

    finished PopulatingNextRightPointer

--- Tree/PopulatingNextRightPointersInEachNode/PopulatingNextRightPointersInEachNode.cpp
+++ Tree/PopulatingNextRightPointersInEachNode/PopulatingNextRightPointersInEachNode.cpp
@@ -2,52 +2,87 @@
 // Author : Yijing Bai
 // Date   : 2016-01-02
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a binary tree
- * 
+ *
  *     struct TreeLinkNode {
  *       TreeLinkNode *left;
  *       TreeLinkNode *right;
  *       TreeLinkNode *next;
  *     }
- * 
- * Populate each next pointer to point to its next right node. If there is no next 
+ *
+ * Populate each next pointer to point to its next right node. If there is no next
  * right node, the next pointer should be set to NULL.
- * 
+ *
  * Initially, all next pointers are set to NULL.
- * 
+ *
  * Note:
- * 
+ *
  * You may only use constant extra space.
- * You may assume that it is a perfect binary tree (ie, all leaves are at the same 
+ * You may assume that it is a perfect binary tree (ie, all leaves are at the same
  * level, and every parent has two children).
- * 
+ *
  * For example,
  * Given the following perfect binary tree,
- * 
+ *
  *          1
  *        /  \
  *       2    3
  *      / \  / \
  *     4  5  6  7
- * 
+ *
  * After calling your function, the tree should look like:
- * 
+ *
  *          1 -> NULL
  *        /  \
  *       2 -> 3 -> NULL
  *      / \  / \
  *     4->5->6->7 -> NULL
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        queue<TreeLinkNode*> node_queue;
+        node_queue.push(root);
+        node_queue.push(NULL);
+        while (!node_queue.empty()) {
+            TreeLinkNode* node = node_queue.front();
+            node_queue.pop();
 
+            if (node) {
+                if (node->left && node->right) {
+                    node_queue.push(node->left);
+                    node_queue.push(node->right);
+                    node->left->next = node->right;
+                    if (node->next) {
+                        node->right->next = node->next->left;
+                    }
+                }
+            } else {
+                if (!node_queue.empty()) {
+                    node_queue.push(NULL);
+                }
+            }
+        }
+    }
+};
 
--- Tree/PopulatingNextRightPointersInEachNode/README.md
+++ Tree/PopulatingNextRightPointersInEachNode/README.md
@@ -1,4 +1,27 @@
 #[PopulatingNextRightPointersInEachNode](https://leetcode.com/problems/populating-next-right-pointers-in-each-node/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `116`
+######Tag: `Tree`
+######Difficulty: `Medium`
+This is the same interview question of TripAdvisor. The key idea is that if the parent
+node is already connect, we can easily use root->next to get to the same level subtree
+and connect the node between. What we need to do is just `cur->right->next = cur->next->left`
+
+However, from https://leetcode.com/discuss/7327/a-simple-accepted-solution
+there is an awesome solution.
+
+```
+void connect(TreeLinkNode *root) {
+    if (root == NULL) return;
+    TreeLinkNode *pre = root;
+    TreeLinkNode *cur = NULL;
+    while(pre->left) {
+        cur = pre;
+        while(cur) {
+            cur->left->next = cur->right;
+            if(cur->next) cur->right->next = cur->next->left;
+            cur = cur->next;
+        }
+        pre = pre->left;
+    }
+}
+```
