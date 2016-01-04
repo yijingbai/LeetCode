49a9e2737458231b6adee1baa79680ef1ff8172d
yijingbai <everatnight@gmail.com>
2016-01-04 10:12:20 -0600

    Finished Perfect Squares

--- DP/PerfectSquares/PerfectSquares.cpp
+++ DP/PerfectSquares/PerfectSquares.cpp
@@ -2,24 +2,31 @@
 // Author : Yijing Bai
 // Date   : 2016-01-04
 
-/********************************************************************************** 
- * 
- * Given a positive integer n, find the least number of perfect square numbers (for 
+/**********************************************************************************
+ *
+ * Given a positive integer n, find the least number of perfect square numbers (for
  * example, 1, 4, 9, 16, ...) which sum to n.
- * 
- * For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 
+ *
+ * For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2
  * because 13 = 4 + 9.
- * 
- * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating 
+ *
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating
  * all test cases.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
 
-
-
+class Solution {
+public:
+    int numSquares(int n) {
+        if (n <= 0) return 0;
+        vector<int> opt(n + 1, INT_MAX);
+        opt[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                opt[i] = min(opt[i], opt[i - j * j] + 1);
+            }
+        }
+        return opt[n];
+    }
+};
--- DP/PerfectSquares/README.md
+++ DP/PerfectSquares/README.md
@@ -1,4 +1,14 @@
 #[PerfectSquares](https://leetcode.com/problems/perfect-squares/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `279`
+######Tag: `DP`
+######Difficulty: `Medium`
+This is a problem that could be solve by DP, math, BFS. For me, the DP is the easiest to understand.
+for a given n, we define that OPT[n] is the least number of perfect squares that sum to n. Then we
+now that:
+
+for all the perfect square j * j that smaller than n, our equation could be:
+
+OPT[n] = min{OPT[n], OPT[n - j * j] + 1}
+
+The explanation for this equation is that we traverse all the square numbers, and if we use it to
+sum to n, then there will be one more number.
