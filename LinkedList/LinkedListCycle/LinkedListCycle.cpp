d539ab18a9086636c6e4d2b769105c3979279233
yijingbai <everatnight@gmail.com>
2016-01-04 23:18:17 -0600

    LinkedListCycle

--- LinkedList/LinkedListCycle/LinkedListCycle.cpp
+++ LinkedList/LinkedListCycle/LinkedListCycle.cpp
@@ -2,21 +2,39 @@
 // Author : Yijing Bai
 // Date   : 2016-01-04
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a linked list, determine if it has a cycle in it.
- * 
+ *
  * Follow up:
  * Can you solve it without using extra space?
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) return false;
 
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
--- LinkedList/LinkedListCycle/README.md
+++ LinkedList/LinkedListCycle/README.md
@@ -1,4 +1,8 @@
 #[LinkedListCycle](https://leetcode.com/problems/linked-list-cycle/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `141`
+######Tag: `LinkedList`
+######Difficulty: `Medium`
+This is a typical question. To finding the cycle, the easiest way is that using two pointer. The slow
+pointer move 1 step each time while the fast pointer move 2 step each time. Since the distance of
+fast pointer and slow pointer will increase by 1 each time, So if there is a loop, the fast pointer
+and slow pointer could finally equal. If the fast pointer equal to NULL, means there is no loop.
