0103d015a58dc9ad40f003acb50cc3b8a5074288
yijingbai <everatnight@gmail.com>
2016-01-04 20:37:03 -0600

    Finished ReorderedList

--- LinkedList/ReorderList/README.md
+++ LinkedList/ReorderList/README.md
@@ -1,4 +1,18 @@
 #[ReorderList](https://leetcode.com/problems/reorder-list/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `143`
+######Tag: `LinkedList`
+######Difficulty: `Medium`
+Initially, I think it pretty easy, since we just need to find the medium node and reverse the right half
+then we done.
+However, when I implement it, I faced many problems.
+First, when finding the medium node, start from `slow = head` and `fast = head` or starting from `slow = head` and `fast = head->next`
+is different. For the First case, when find the medium node, like 1->2, the Medium node will be 2. And for the second one,
+the medium node will be 1. It could totally make the difference.
+
+The second problem is that I want to make the list like 1->2<-3, but it is absolte the bad choice since when the node
+number is even 1->2->3->4, it is hard for us to judge the end. The easiest way is that from the median, split the
+list into two. For example, for the 1->2, median is 1, we split to 1 and 2, and for 1->2->3, median is 2, and we split to
+1->2 and 3. It is much easier to us. Since we just need to judge the NULL pointer.
+
+The third problem is that when traverse is end, we may ignore the end of the list, which will cause the list never end.
+so remember to set the last element to NULL to make the list end;
--- LinkedList/ReorderList/ReorderList.cpp
+++ LinkedList/ReorderList/ReorderList.cpp
@@ -2,21 +2,113 @@
 // Author : Yijing Bai
 // Date   : 2016-01-04
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
  * reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
- * 
+ *
  * You must do this in-place without altering the nodes' values.
- * 
+ *
  * For example,
  * Given {1,2,3,4}, reorder it to {1,4,2,3}.
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
+#include <iostream>
+
+/**
+Definition for singly-linked list.
+ */
+struct ListNode {
+    i
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        ListNode* begin;
+        ListNode* end;
+        ListNode* dummy_head;
+        ListNode* slow;
+        ListNode* fast;
+        ListNode* tmp;
+        dummy_head = new ListNode(-1);
+        begin = head;
+        slow = head;
+        fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (slow) {
+            fast = slow->next;
+            slow->next = NULL;
+
+            slow = fast;
+            fast = fast->next;
+            slow->next = NULL;
+        }
+
+        while (fast) {
+            tmp = fast->next;
+            fast->next = slow;
+            slow = fast;
+            fast = tmp;
+        }
+
+        end = slow;
+
+        ListNode* dummy_head_p = dummy_head;
+
+        while (begin || end) {
+
+            if (begin) {
+                dummy_head_p->next = begin;
+                begin = begin->next;
+                dummy_head_p = dummy_head_p->next;
+            }
 
 
+            if (end) {
+                dummy_head_p->next = end;
+                end = end->next;
+                dummy_head_p = dummy_head_p->next;
+            }
+        }
 
+        dummy_head_p->next = NULL;
+
+        head->next = dummy_head->next->next;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    ListNode* n2 = new ListNode(2);
+    ListNode* n3 = new ListNode(3);
+    ListNode* n4 = new ListNode(4);
+    head->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+
+    Solution s;
+    s.reorderList(head);
+
+}
