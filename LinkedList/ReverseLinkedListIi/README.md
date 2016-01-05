ce114f5aaee4489f02a96cb2aaf8d4e8b9480683
yijingbai <everatnight@gmail.com>
2016-01-04 22:21:48 -0600

    Finished ReverseLinkedListII

--- LinkedList/ReverseLinkedListIi/README.md
+++ LinkedList/ReverseLinkedListIi/README.md
@@ -1,4 +1,21 @@
 #[ReverseLinkedListIi](https://leetcode.com/problems/reverse-linked-list-ii/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `92`
+######Tag: `LinkedList`
+######Difficulty: `Medium`
+
+This is the first medium question that I can right in one shot!
+The idea is simple. And the tricky part is that range.
+
+There are two cases:
+1. m == 1. For this case, the first node will be reversed. Therefore, we can not return it as the head node.
+In his case, the head node will be the node at index n, so we return it as the head.
+2. m != 1. For this case, we just return first node as the head node.
+
+In addition, the sequence of
+
+```
+prev->next = left;
+reverse_end->next = right;
+```
+is important. If we swap them, it will be wrong, since when m == 1, the prev == reverse_end. Its next should point
+to right, not left. 
--- LinkedList/ReverseLinkedListIi/ReverseLinkedListIi.cpp
+++ LinkedList/ReverseLinkedListIi/ReverseLinkedListIi.cpp
@@ -2,27 +2,66 @@
 // Author : Yijing Bai
 // Date   : 2016-01-04
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Reverse a linked list from position m to n. Do it in-place and in one-pass.
- * 
+ *
  * For example:
  * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
- * 
+ *
  * return 1->4->3->2->5->NULL.
- * 
+ *
  * Note:
  * Given m, n satisfy the following condition:
  * 1 ≤ m ≤ n ≤ length of list.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (!head || !head->next) return head;
 
+        int index = 1;
+        ListNode* left = head;
+        ListNode* right;
+        ListNode* prev = left;
+        while (index < m) {
+            prev = left;
+            left = left->next;
+            ++index;
+        }
+
+        ListNode* reverse_end = left;
+
+        right = left->next;
+
+        int index_right = index + 1;
+        ListNode* tmp;
+        while (index_right <= n) {
+            tmp = right->next;
+            right->next = left;
+            left = right;
+            right = tmp;
+            ++index_right;
+        }
+
+        prev->next = left;
+        reverse_end->next = right;
+
+        if (m == 1) {
+            return left;
+        } else {
+            return head;
+        }
+    }
+};
