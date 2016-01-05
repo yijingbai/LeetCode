ad4e6a1991681fc7b969cdf8ea077212cc11f5d8
yijingbai <everatnight@gmail.com>
2016-01-05 13:21:14 -0600

    Finished RemoveDuplcatesFrom sortedList

--- LinkedList/RemoveDuplicatesFromSortedListIi/README.md
+++ LinkedList/RemoveDuplicatesFromSortedListIi/README.md
@@ -1,4 +1,32 @@
 #[RemoveDuplicatesFromSortedListIi](https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `82`
+######Tag: `LinkedList`
+######Difficulty: `Medium`
+Considering the corner case of the first node will be deleted, using a dummy head will be the best choice for
+us.
+The idea is that creating a dummy head and only add the node that the show count is 1. 
+Cited from https://leetcode.com/discuss/12724/my-accepted-java-code , there is a easier thinking way is that
+just compare the current and current->next if different, added, otherwise, keeping going next.
+
+```
+public ListNode deleteDuplicates(ListNode head) {
+        if(head==null) return null;
+        ListNode FakeHead=new ListNode(0);
+        FakeHead.next=head;
+        ListNode pre=FakeHead;
+        ListNode cur=head;
+        while(cur!=null){
+            while(cur.next!=null&&cur.val==cur.next.val){
+                cur=cur.next;
+            }
+            if(pre.next==cur){
+                pre=pre.next;
+            }
+            else{
+                pre.next=cur.next;
+            }
+            cur=cur.next;
+        }
+        return FakeHead.next;
+    }
+```
--- LinkedList/RemoveDuplicatesFromSortedListIi/RemoveDuplicatesFromSortedListIi.cpp
+++ LinkedList/RemoveDuplicatesFromSortedListIi/RemoveDuplicatesFromSortedListIi.cpp
@@ -2,20 +2,65 @@
 // Author : Yijing Bai
 // Date   : 2016-01-05
 
-/********************************************************************************** 
- * 
- * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving 
+/**********************************************************************************
+ *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving
  * only distinct numbers from the original list.
- * 
+ *
  * For example,
  * Given 1->2->3->3->4->4->5, return 1->2->5.
  * Given 1->1->1->2->3, return 2->3.
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
 
+        ListNode* current = head;
+        ListNode* prev = head;
+        ListNode* new_next = NULL;
+        ListNode* new_head = NULL;
+
+        ListNode* dummy_head = new ListNode(-1);
+        ListNode* dp = dummy_head;
+
+        int prev_val = head->val;
+        int count = 0;
+
+        while (current) {
+            if (current->val != prev_val) {
+                prev_val = current->val;
+                if (count == 1) {
+                    dp->next = prev;
+                    dp = dp->next;
+                    dp->next = NULL;
+                }
+                prev = current;
+                count = 1;
+            } else {
+                ++count;
+            }
+
+            current = current->next;
+        }
+
+        if (count == 1) {
+            dp->next = prev;
+            dp = dp->next;
+            dp->next = NULL;
+        }
+
+        return dummy_head->next;
+    }
+};
