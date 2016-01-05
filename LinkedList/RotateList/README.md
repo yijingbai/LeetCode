01a8c11af86299940310d117296bfb9559a2f292
yijingbai <everatnight@gmail.com>
2016-01-04 21:34:34 -0600

    Finished RotateList

--- LinkedList/RotateList/README.md
+++ LinkedList/RotateList/README.md
@@ -1,4 +1,13 @@
 #[RotateList](https://leetcode.com/problems/rotate-list/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `61`
+######Tag: `LinkedList`
+######Difficulty: `Medium`
+The meaning of rotate is confusing initially. We can imaging that we can move the list from left to the right,
+at mean time, the node at the end we come back to the beginning. 
+Therefore, we should first count the number of nodes of the list. Then we connect the last node to the first
+node. What we need to do next is that calculate the k = k % length. The reason is that k could bigger than
+the length. But if k == length, it is same to not rotate the list. So we just need to get the mod value.
+
+Then we need to find the new head. The way is that we find the new end. We just need to move the pointer
+lenth - k times and it will point to the end of the list. Then we set its next as the beginning and
+set it next as NULL.
--- LinkedList/RotateList/RotateList.cpp
+++ LinkedList/RotateList/RotateList.cpp
@@ -2,21 +2,54 @@
 // Author : Yijing Bai
 // Date   : 2016-01-04
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a list, rotate the list to the right by k places, where k is non-negative.
- * 
+ *
  * For example:
  * Given 1->2->3->4->5->NULL and k = 2,
  * return 4->5->1->2->3->NULL.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return head;
 
+        ListNode* tail = head;
+        int length = 1;
+        while(tail->next) {
+            tail = tail->next;
+            ++length;
+        }
+
+        tail->next = head;
+
+        k %= length;
+        for (int i = 0; i < length - k; ++i) {
+            tail = tail->next;
+        }
+
+        ListNode* new_head = tail->next;
+        tail->next = NULL;
+
+        return new_head;
+
+    }
+};
