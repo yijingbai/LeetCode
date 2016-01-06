9861fe4dc4bbd01babe4ab1728005e8ce0319a8a
yijingbai <everatnight@gmail.com>
2016-01-06 16:35:53 -0600

    Finished FractionToRecurringDecimal

--- HashMap/FractionToRecurringDecimal/FractionToRecurringDecimal.cpp
+++ HashMap/FractionToRecurringDecimal/FractionToRecurringDecimal.cpp
@@ -2,26 +2,69 @@
 // Author : Yijing Bai
 // Date   : 2016-01-06
 
-/********************************************************************************** 
- * 
- * Given two integers representing the numerator and denominator of a fraction, return 
+/**********************************************************************************
+ *
+ * Given two integers representing the numerator and denominator of a fraction, return
  * the fraction in string format.
- * 
+ *
  * If the fractional part is repeating, enclose the repeating part in parentheses.
- * 
+ *
  * For example,
- * 
+ *
  * Given numerator = 1, denominator = 2, return "0.5".
  * Given numerator = 2, denominator = 1, return "2".
  * Given numerator = 2, denominator = 3, return "0.(6)".
- * 
- * Credits:Special thanks to @Shangrila for adding this problem and creating all test 
+ *
+ * Credits:Special thanks to @Shangrila for adding this problem and creating all test
  * cases.
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    string fractionToDecimal(int n, int d) {
+    // zero numerator
+    if (n == 0) return "0";
 
+    string res;
+    // determine the sign
+    if (n < 0 ^ d < 0) res += '-';
+
+    // remove sign of operands
+    n = abs(n), d = abs(d);
+
+    // append integral part
+    res += to_string(n / d);
+
+    // in case no fractional part
+    if (n % d == 0) return res;
+
+    res += '.';
+
+    unordered_map<int, int> map;
+
+    // simulate the division process
+    for (int64_t r = n % d; r; r %= d) {
+
+        // meet a known remainder
+        // so we reach the end of the repeating part
+        if (map.count(r) > 0) {
+            res.insert(map[r], 1, '(');
+            res += ')';
+            break;
+        }
+
+        // the remainder is first seen
+        // remember the current position for it
+        map[r] = res.size();
+
+        r *= 10;
+
+        // append the quotient digit
+        res += to_string(r / d);
+    }
+
+    return res;
+}
+};
--- HashMap/FractionToRecurringDecimal/README.md
+++ HashMap/FractionToRecurringDecimal/README.md
@@ -1,4 +1,21 @@
 #[FractionToRecurringDecimal](https://leetcode.com/problems/fraction-to-recurring-decimal/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `166`
+######Tag: `HashMap`
+######Difficulty: `Medium`
+I did not finished this problem by myself.
+
+The main idea is that to mock the divide process. And the most important problem to solve is is identify the recurring parts.
+Cited From https://leetcode.com/discuss/42159/0ms-c-solution-with-detailed-explanations , it has very good explanation.
+
+```
+Well, the key to this problem is on how to identify the recurring parts. After doing some examples using pen and paper, you may find that for the decimal parts to recur, the remainders should recur. So we need to maintain the remainders we have seen. Once we see a repeated remainder, we know that we have reached the end of the recurring parts and should enclose it with a ). However, we still need to insert the ( to the correct position. So we maintain a mapping from each remainder to the position of the corresponding quotient digit of it in the recurring parts. Then we use this mapping to retrieve the starting position of the recurring parts.
+
+Now we have solved the trickiest part of this problem.
+
+There are some remaining problems to solve to achieve a bug-free solution.
+
+Pay attention to the sign of the result;
+Handle cases that may cause overflow like numerator = -2147483648, denominator = -1 appropriately by using long long;
+Handle all the cases of (1) no fractional part; (2) fractional part does not recur; and (3) fractional part recurs respectively.
+To handle problem 3, we divide the division process into the integral part and the fractional part. For the fractional part, if it does not recur, then the remainder will become 0 at some point and we could return. If it does recur, the method metioned in the first paragraph has already handled it.
+```
