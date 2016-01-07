f2a0f727db63f32cbf5f4d686ce11e9fb4f8a5de
yijingbai <everatnight@gmail.com>
2016-01-06 19:50:02 -0600

    Finished GroupAnagrams

--- HashMap/Anagrams/Anagrams.cpp
+++ HashMap/Anagrams/Anagrams.cpp
@@ -2,32 +2,61 @@
 // Author : Yijing Bai
 // Date   : 2016-01-06
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given an array of strings, group anagrams together.
- * 
- * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+ *
+ * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
  * Return:
- * 
+ *
  * [
  *   ["ate", "eat","tea"],
  *   ["nat","tan"],
  *   ["bat"]
  * ]
- * 
+ *
  * Note:
- * 
+ *
  * For the return value, each inner list's elements must follow the lexicographic order.
  * All inputs will be in lower-case.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        if (strs.size() == 0) return result;
+
+        unordered_map<string, vector<string>> str_map;
+        for (string& str : strs) {
+            string origin = str;
+            sort(str.begin(), str.end());
+            if (str_map.find(str) != str_map.end()) {
+                str_map[str].push_back(origin);
+            } else {
+                str_map[str] = vector<string>();
+            }
+        }
+
+        for (auto iter = str_map.begin(); iter != str_map.end(); ++iter) {
+            sort(iter->second.begin(), iter->second.end());
+            result.push_back(iter->second);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+	Solution s;
+	vector<string> input{""};
+	s.groupAnagrams(input);
+
+}
--- HashMap/Anagrams/README.md
+++ HashMap/Anagrams/README.md
@@ -1,4 +1,10 @@
 #[Anagrams](https://leetcode.com/problems/anagrams/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `49`
+######Tag: `HashMap String`
+######Difficulty: `Medium`
+Initionally I have no idea with this problem.
+After cited from https://leetcode.com/discuss/58561/share-my-short-java-solution
+The idea is pretty simple. Since each group contains all the anagram of the string. If we sort them,
+they actually is the same. Therefore, we just use the sorted string as the key and the anagram list
+as the value. If found the key, push the string into the list. Otherwise, make a new list and put
+the element in.
