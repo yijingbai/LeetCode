81de6c63fbe4f4174b7662ee9e7a47b9e8f60f4f
yijingbai <everatnight@gmail.com>
2016-01-06 21:14:21 -0600

    Finished HIndex

--- HashMap/HIndex/HIndex.cpp
+++ HashMap/HIndex/HIndex.cpp
@@ -2,37 +2,47 @@
 // Author : Yijing Bai
 // Date   : 2016-01-06
 
-/********************************************************************************** 
- * 
- * Given an array of citations (each citation is a non-negative integer) of a 
+/**********************************************************************************
+ *
+ * Given an array of citations (each citation is a non-negative integer) of a
  * researcher, write a function to compute the researcher's h-index.
- * 
- * According to the definition of h-index on Wikipedia: "A scientist has index h if h 
- * of his/her N papers have at least h citations each, and the other N − h papers have 
+ *
+ * According to the definition of h-index on Wikipedia: "A scientist has index h if h
+ * of his/her N papers have at least h citations each, and the other N − h papers have
  * no more than h citations each."
- * 
- * For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 
- * papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. 
- * Since the researcher has 3 papers with at least 3 citations each and the remaining 
+ *
+ * For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5
+ * papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+ * Since the researcher has 3 papers with at least 3 citations each and the remaining
  * two with no more than 3 citations each, his h-index is 3.
- * 
- * Note: If there are several possible values for h, the maximum one is taken as the 
+ *
+ * Note: If there are several possible values for h, the maximum one is taken as the
  * h-index.
- * 
+ *
  *   An easy approach is to sort the array first.
  *   What are the possible values of h-index?
  *   A faster approach is to use extra space.
- * 
- * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating 
+ *
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating
  * all test cases.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(), h = 0;
+        int* counts = new int[n + 1]();
+        for (int c : citations) {
+            counts[min(c, n)]++;
+        }
 
+        for (int i = n; i; i--) {
+            h += counts[i];
+            if (h >= i) return i;
+        }
+        return h;
+
+    }
+};
--- HashMap/HIndex/README.md
+++ HashMap/HIndex/README.md
@@ -1,4 +1,13 @@
 #[HIndex](https://leetcode.com/problems/h-index/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `274`
+######Tag: `HashMap`
+######Difficulty: `Medium`
+I did not come up the solution by myself.
+
+The idea is that, first, we know that the h-index >= 1 && h-index < citation.size(). 
+And also, considering the definition of the h-index: h of N papers have at least h citations each.
+We know that if the citations >= citation.size(), we just set at occursion times to ctation.size().
+In the first iteration, we count the show times of each number. If it >= N, count it to N.
+Then for the second iteration, we start from MAX possible value h. And sum the citation num.
+if the citation num >= h, measn have at lest h citation each. Then we return h. 
+If all the citation num < h, we just return the citenum at last.
