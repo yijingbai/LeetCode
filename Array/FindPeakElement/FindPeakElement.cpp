717bd9b5e4442cdfd0c66b0adc71090b5d82757a
yijingbai <everatnight@gmail.com>
2016-01-08 17:43:23 -0600

    Finished FindPeakElement

--- Array/FindPeakElement/FindPeakElement.cpp
+++ Array/FindPeakElement/FindPeakElement.cpp
@@ -2,32 +2,47 @@
 // Author : Yijing Bai
 // Date   : 2016-01-08
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * A peak element is an element that is greater than its neighbors.
- * 
- * Given an input array where num[i] ≠ num[i+1], find a peak element and return its 
+ *
+ * Given an input array where num[i] ≠ num[i+1], find a peak element and return its
  * index.
- * 
- * The array may contain multiple peaks, in that case return the index to any one of 
+ *
+ * The array may contain multiple peaks, in that case return the index to any one of
  * the peaks is fine.
- * 
+ *
  * You may imagine that num[-1] = num[n] = -∞.
- * 
- * For example, in array [1, 2, 3, 1], 3 is a peak element and your function should 
+ *
+ * For example, in array [1, 2, 3, 1], 3 is a peak element and your function should
  * return the index number 2.
- * 
+ *
  * click to show spoilers.
- * 
+ *
  * Note:
  * Your solution should be in logarithmic complexity.
- * 
+ *
  * Credits:Special thanks to @ts for adding this problem and creating all test cases.
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
 
+        while (low < high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = mid1 + 1;
+            if (nums[mid1] < nums[mid2]) {
+                low = mid2;
+            } else {
+                high = mid1;
+            }
+        }
+        return low;
+    }
+};
--- Array/FindPeakElement/README.md
+++ Array/FindPeakElement/README.md
@@ -1,4 +1,8 @@
 #[FindPeakElement](https://leetcode.com/problems/find-peak-element/)
-######No: ``
-######Tag: ``
-######Difficulty: ``
+######No: `162`
+######Tag: `Array BinarySearch`
+######Difficulty: `Medium`
+We can solve this problem using binary search. Binary search could not only been used in the sorted array but
+also used in an unsorted array. In this problem, we could use binary search to find a local peak.
+After we get the element of the middle, we just need to compare the num[mid] and num[mid + 1].
+The problem is that using this method could only find a local peak instead of the global peak.
