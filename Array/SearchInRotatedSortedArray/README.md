5d4ee50424878419b2b688e0d323686db0c2c6d8
yijingbai <everatnight@gmail.com>
2016-01-08 20:42:03 -0600

    Finished SearchInRotatedSortedArray

--- Array/SearchInRotatedSortedArray/README.md
+++ Array/SearchInRotatedSortedArray/README.md
@@ -1,4 +1,15 @@
 #[SearchInRotatedSortedArray](https://leetcode.com/problems/search-in-rotated-sorted-array/)
-######No: ``
-######Tag: ``
+######No: `33`
+######Tag: `Array`
 ######Difficulty: `Medium`
+This is a Binary Search Problem. But what is different from the ordinary binary search is that
+not the total array is sorted since the array is rotated. The way to deal with this like the
+the question before:
+
+```
+1) everytime check if targe == nums[mid], if so, we find it.
+2) otherwise, we check if the first half is in order (i.e. nums[left]<=nums[mid]) 
+  and if so, go to step 3), otherwise, the second half is in order,   go to step 4)
+3) check if target in the range of [left, mid-1] (i.e. nums[left]<=target < nums[mid]), if so, do search in the first half, i.e. right = mid-1; otherwise, search in the second half left = mid+1;
+4)  check if target in the range of [mid+1, right] (i.e. nums[mid]<target <= nums[right]), if so, do search in the second half, i.e. left = mid+1; otherwise search in the first half right = mid-1;
+```
--- Array/SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
+++ Array/SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
@@ -2,24 +2,49 @@
 // Author : Yijing Bai
 // Date   : 2016-01-08
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
- * 
+ *
  * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
- * 
- * You are given a target value to search. If found in the array return its index, 
+ *
+ * You are given a target value to search. If found in the array return its index,
  * otherwise return -1.
- * 
+ *
  * You may assume no duplicate exists in the array.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 0) return -1;
 
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) return mid;
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> input{3, 1};
+    Solution s;
+    s.search(input, 1);
+}
