2662a2ecefa72022f6996ec3089c5d0b8623615a
yijingbai <everatnight@gmail.com>
2016-01-08 18:24:11 -0600

    update name

---
+++ Array/SearchInRotatedSortedArrayII/README.md
@@ -0,0 +1,4 @@
+#[SearchInRotatedSortedArrayIi](https://leetcode.com/problems/search-in-rotated-sorted-array-ii/)
+######No: ``
+######Tag: ``
+######Difficulty: ``
---
+++ Array/SearchInRotatedSortedArrayII/SearchInRotatedSortedArrayIi.cpp
@@ -0,0 +1,23 @@
+// Source : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// Author : Yijing Bai
+// Date   : 2016-01-08
+
+/********************************************************************************** 
+ * 
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ * 
+ * Would this affect the run-time complexity? How and why?
+ * 
+ * Write a function to determine if a given target is in the array.
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
