a789f99ae5d2ed047f1b18ce78bf2ee1966e9112
yijingbai <everatnight@gmail.com>
2016-01-09 20:27:11 -0600

    Added RestoreIP

---
+++ String/RestoreIpAddresses/README.md
@@ -0,0 +1,4 @@
+#[RestoreIpAddresses](https://leetcode.com/problems/restore-ip-addresses/)
+######No: ``
+######Tag: ``
+######Difficulty: `Medium`
---
+++ String/RestoreIpAddresses/RestoreIpAddresses.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/restore-ip-addresses/
+// Author : Yijing Bai
+// Date   : 2016-01-09
+
+/********************************************************************************** 
+ * 
+ * Given a string containing only digits, restore it by returning all possible valid IP 
+ * address combinations.
+ * 
+ * For example:
+ * Given "25525511135",
+ * 
+ * return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
