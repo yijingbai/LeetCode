3a9bbf8b31c5ef06d9e92f18ab0928a10f37ccaf
yijingbai <everatnight@gmail.com>
2016-01-10 16:06:38 -0600

    Finished CoinChange

--- DP/CoinChange/CoinChange.cpp
+++ DP/CoinChange/CoinChange.cpp
@@ -2,31 +2,45 @@
 // Author : Yijing Bai
 // Date   : 2016-01-10
 
-/********************************************************************************** 
- * 
- * You are given coins of different denominations and a total amount of money amount. 
- * Write a function to compute the fewest number of coins that you need to make up that 
- * amount. If that amount of money cannot be made up by any combination of the coins, 
+/**********************************************************************************
+ *
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that
+ * amount. If that amount of money cannot be made up by any combination of the coins,
  * return -1.
- * 
+ *
  * Example 1:
  * coins = [1, 2, 5], amount = 11
  * return 3 (11 = 5 + 5 + 1)
- * 
+ *
  * Example 2:
  * coins = [2], amount = 3
  * return -1.
- * 
+ *
  * Note:
  * You may assume that you have an infinite number of each kind of coin.
- * 
- * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating 
+ *
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating
  * all test cases.
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int Max = amount + 1;
+        vector<int> opt(amount + 1, Max);
+        opt[0] = 0;
 
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (i - c >= 0) {
+                    opt[i] = min(opt[i], opt[i - c] + 1);
+                }
+            }
+        }
+
+        return opt[amount] > amount ? -1 : opt[amount];
+    }
+};
--- DP/CoinChange/README.md
+++ DP/CoinChange/README.md
@@ -1,4 +1,16 @@
 #[CoinChange](https://leetcode.com/problems/coin-change/)
-######No: ``
-######Tag: ``
+######No: `322`
+######Tag: `DP`
 ######Difficulty: `Medium`
+
+This is a typical and easy DP problem.
+The equation is
+```
+OPT[i] means the fewest number of coins to make up the amount i.
+
+OPT[i] = min{
+For all the coin value c in coins
+OPT[i], OPT[i - c] + 1
+}
+
+```
