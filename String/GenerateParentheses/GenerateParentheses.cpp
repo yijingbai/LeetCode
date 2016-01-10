613bfc866bf03f56b4932734cc8bf7d02bede67f
yijingbai <everatnight@gmail.com>
2016-01-10 13:10:34 -0600

    Generate Parentheses

--- String/GenerateParentheses/GenerateParentheses.cpp
+++ String/GenerateParentheses/GenerateParentheses.cpp
@@ -2,23 +2,32 @@
 // Author : Yijing Bai
 // Date   : 2016-01-10
 
-/********************************************************************************** 
- * 
- * Given n pairs of parentheses, write a function to generate all combinations of 
+/**********************************************************************************
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations of
  * well-formed parentheses.
- * 
+ *
  * For example, given n = 3, a solution set is:
- * 
+ *
  * "((()))", "(()())", "(())()", "()(())", "()()()"
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        if (n == 0) return res;
+        generate_helper(res, "", n, 0);
+        return res;
+    }
 
+    void generate_helper(vector<string>& res, string val, int n, int m) {
+        if (n == 0 && m == 0) {res.push_back(val); return;}
+
+        if (m > 0) generate_helper(res, val + ')', n, m - 1);
+        if (n > 0) generate_helper(res, val + '(', n - 1, m + 1);
+
+    }
+};
--- String/GenerateParentheses/README.md
+++ String/GenerateParentheses/README.md
@@ -1,4 +1,8 @@
 #[GenerateParentheses](https://leetcode.com/problems/generate-parentheses/)
-######No: ``
-######Tag: ``
+######No: `22`
+######Tag: `String`
 ######Difficulty: `Medium`
+
+This is simple backtracking problem. The idea is that using m and n to track the
+number of '(' and number of ')' respectively. And when n == 0 and m == 0, there
+will one more solution.
