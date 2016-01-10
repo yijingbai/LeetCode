fef019fa369c14f4120b6e49fe1ea45f94049715
yijingbai <everatnight@gmail.com>
2016-01-10 13:20:40 -0600

    Added Simplify Path

---
+++ String/SimplifyPath/README.md
@@ -0,0 +1,4 @@
+#[SimplifyPath](https://leetcode.com/problems/simplify-path/)
+######No: ``
+######Tag: ``
+######Difficulty: `Medium`
---
+++ String/SimplifyPath/SimplifyPath.cpp
@@ -0,0 +1,30 @@
+// Source : https://leetcode.com/problems/simplify-path/
+// Author : Yijing Bai
+// Date   : 2016-01-10
+
+/********************************************************************************** 
+ * 
+ * Given an absolute path for a file (Unix-style), simplify it.
+ * 
+ * For example,
+ * path = "/home/", => "/home"
+ * path = "/a/./b/../../c/", => "/c"
+ * 
+ * click to show corner cases.
+ * 
+ * Corner Cases:
+ * 
+ * Did you consider the case where path = "/../"?
+ * In this case, you should return "/".
+ * Another corner case is the path might contain multiple slashes '/' together, such as 
+ * "/home//foo/".
+ * In this case, you should ignore redundant slashes and return "/home/foo".
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
