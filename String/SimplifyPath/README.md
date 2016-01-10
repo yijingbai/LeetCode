f2f5ddccb90207ed86bae27a9f0a6bda235cd03f
yijingbai <everatnight@gmail.com>
2016-01-10 13:25:28 -0600

    Finished Simplify Path

--- String/SimplifyPath/README.md
+++ String/SimplifyPath/README.md
@@ -1,4 +1,9 @@
 #[SimplifyPath](https://leetcode.com/problems/simplify-path/)
-######No: ``
-######Tag: ``
+######No: `71`
+######Tag: `Medium`
 ######Difficulty: `Medium`
+
+The idea for this problem is simple. Just using a stack to keep track the full path.
+And when there is a `..` just pop out the element. But be careful, when pop out the
+element, we need to check the size of the stack. When the stack is empty, we should
+not pop.
--- String/SimplifyPath/SimplifyPath.cpp
+++ String/SimplifyPath/SimplifyPath.cpp
@@ -2,29 +2,53 @@
 // Author : Yijing Bai
 // Date   : 2016-01-10
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given an absolute path for a file (Unix-style), simplify it.
- * 
+ *
  * For example,
  * path = "/home/", => "/home"
  * path = "/a/./b/../../c/", => "/c"
- * 
+ *
  * click to show corner cases.
- * 
+ *
  * Corner Cases:
- * 
+ *
  * Did you consider the case where path = "/../"?
  * In this case, you should return "/".
- * Another corner case is the path might contain multiple slashes '/' together, such as 
+ * Another corner case is the path might contain multiple slashes '/' together, such as
  * "/home//foo/".
  * In this case, you should ignore redundant slashes and return "/home/foo".
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    string simplifyPath(string path) {
 
+        stringstream ss(path);
+        string item;
+        vector<string> paths;
+        while (getline(ss, item, '/')) {
+            if (item == "" || item == ".") continue;
+            if (item == "..") {
+                if (!paths.empty()) paths.pop_back();
+                continue;
+            } else {
+                paths.push_back(item);
+            }
+        }
+
+        string res;
+
+        for (auto v : paths) {
+            res += "/" + v;
+        }
+
+        if (res == "") res += "/";
+
+        return res;
+
+    }
+};
