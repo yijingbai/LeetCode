cb90e602e6f6a67c39cc333cf0aac6b3cfd3a667
yijingbai <everatnight@gmail.com>
2016-01-13 20:58:16 -0600

    Finished Combinations

--- BackTracking/Combinations/Combinations.cpp
+++ BackTracking/Combinations/Combinations.cpp
@@ -2,14 +2,14 @@
 // Author : Yijing Bai
 // Date   : 2016-01-13
 
-/********************************************************************************** 
- * 
- * Given two integers n and k, return all possible combinations of k numbers out of 1 
+/**********************************************************************************
+ *
+ * Given two integers n and k, return all possible combinations of k numbers out of 1
  * ... n.
- * 
+ *
  * For example,
  * If n = 4 and k = 2, a solution is:
- * 
+ *
  * [
  *   [2,4],
  *   [3,4],
@@ -18,15 +18,39 @@
  *   [1,3],
  *   [1,4],
  * ]
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
+#include <vector>
 
+using namespace std;
 
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> cur;
+        if (n < k) return res;
 
+        combine_helper(n, k, 1, res, cur);
+        return res;
+    }
+
+    void combine_helper(int n, int k, int x, vector<vector<int>>& res, vector<int>& cur) {
+        if (cur.size() == k) {
+            res.push_back(cur);
+            return;
+        }
+
+        for (int i = x; i <= n; i++) {
+            cur.push_back(i);
+            combine_helper(n, k, i + 1, res, cur);
+            cur.pop_back();
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> res = s.combine(10, 7);
+}
