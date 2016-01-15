2a95e978a7e3dcd6df5fd6e987a049b5391b64f2
yijingbai <everatnight@gmail.com>
2016-01-14 21:30:49 -0600

    Finished GasStatsion

--- Greedy/GasStation/GasStation.cpp
+++ Greedy/GasStation/GasStation.cpp
@@ -2,26 +2,45 @@
 // Author : Yijing Bai
 // Date   : 2016-01-14
 
-/********************************************************************************** 
- * 
- * There are N gas stations along a circular route, where the amount of gas at station 
+/**********************************************************************************
+ *
+ * There are N gas stations along a circular route, where the amount of gas at station
  * i is gas[i].
- * 
- * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from 
- * station i to its next station (i+1). You begin the journey with an empty tank at one 
+ *
+ * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from
+ * station i to its next station (i+1). You begin the journey with an empty tank at one
  * of the gas stations.
- * 
- * Return the starting gas station's index if you can travel around the circuit once, 
+ *
+ * Return the starting gas station's index if you can travel around the circuit once,
  * otherwise return -1.
- * 
+ *
  * Note:
  * The solution is guaranteed to be unique.
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sum_gas = 0, sum_cost = 0, start = 0, tank = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            sum_gas += gas[i];
+            sum_cost += cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        if (sum_gas < sum_cost) return -1;
+        else return start;
+    }
+
+};
 
--- Greedy/GasStation/README.md
+++ Greedy/GasStation/README.md
@@ -1,4 +1,34 @@
 #[GasStation](https://leetcode.com/problems/gas-station/)
-######No: ``
-######Tag: ``
+######No: `134`
+######Tag: `Greedy`
 ######Difficulty: `Medium`
+I did not finished this problem by myself. There are two approach:
+```
+I have thought for a long time and got two ideas:
+
+If car starts at A and can not reach B. Any station between A and B can not reach B.(B is the first station that A can not reach.)
+If the total number of gas is bigger than the total number of cost. There must be a solution.
+(Should I prove them?)
+Here is my solution based on those ideas:
+
+public int canCompleteCircuit(int[] gas, int[] cost) {
+    int sumGas = 0;
+    int sumCost = 0;
+    int start = 0;
+    int tank = 0;
+    for (int i = 0; i < gas.length; i++) {
+        sumGas += gas[i];
+        sumCost += cost[i];
+        tank += gas[i] - cost[i];
+        if (tank < 0) {
+            start = i + 1;
+            tank = 0;
+        }
+    }
+    if (sumGas < sumCost) {
+        return -1;
+    } else {
+        return start;
+    }
+}
+```
