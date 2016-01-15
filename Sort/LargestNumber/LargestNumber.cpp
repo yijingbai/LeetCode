59130da56dc0c5bef294835c71f34e05d7e400e5
yijingbai <everatnight@gmail.com>
2016-01-14 23:14:14 -0600

    Finished LargetNumber

--- Sort/LargestNumber/LargestNumber.cpp
+++ Sort/LargestNumber/LargestNumber.cpp
@@ -2,22 +2,34 @@
 // Author : Yijing Bai
 // Date   : 2016-01-14
 
-/********************************************************************************** 
- * 
- * Given a list of non negative integers, arrange them such that they form the largest 
+/**********************************************************************************
+ *
+ * Given a list of non negative integers, arrange them such that they form the largest
  * number.
- * 
+ *
  * For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
- * 
- * Note: The result may be very large, so you need to return a string instead of an 
+ *
+ * Note: The result may be very large, so you need to return a string instead of an
  * integer.
- * 
+ *
  * Credits:Special thanks to @ts for adding this problem and creating all test cases.
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
-
+class Solution {
+public:
+    string largestNumber(vector<int> &num) {
+        sort(num.begin(), num.end(), [](int a, int b){
+            return to_string(a)+to_string(b) > to_string(b)+to_string(a);
+        });
+        string ans;
+        for(int i=0; i<num.size(); i++){
+            ans += to_string(num[i]);
+        }
+        return ans[0]=='0' ? "0" : ans;
+    }
+};
--- Sort/LargestNumber/README.md
+++ Sort/LargestNumber/README.md
@@ -1,4 +1,6 @@
 #[LargestNumber](https://leetcode.com/problems/largest-number/)
-######No: ``
-######Tag: ``
+######No: `179`
+######Tag: `Sort`
 ######Difficulty: `Medium`
+Cited from https://leetcode.com/problems/largest-number/ there is a great and neat solution for this.
+The key idea is replacing the comparitor with the rule that to_strng(a) + to__strng(b) > to_string(b) + to_string(a).
