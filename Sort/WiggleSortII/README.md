9ea61bac65c3b57c35f5fc860d4ee05e8f69136a
yijingbai <everatnight@gmail.com>
2016-01-14 22:46:46 -0600

    Added wiggle sort ii

---
+++ Sort/WiggleSortII/README.md
@@ -0,0 +1,4 @@
+#[WiggleSortIi](https://leetcode.com/problems/wiggle-sort-ii/)
+######No: ``
+######Tag: ``
+######Difficulty: `Medium`
---
+++ Sort/WiggleSortII/WiggleSortII.cpp
@@ -0,0 +1,32 @@
+// Source : https://leetcode.com/problems/wiggle-sort-ii/
+// Author : Yijing Bai
+// Date   : 2016-01-14
+
+/********************************************************************************** 
+ * 
+ * Given an unsorted array nums, reorder it such that
+ *     nums[0]  nums[2] .
+ * 
+ *     Example:
+ *     (1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6]. 
+ *     (2) Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
+ * 
+ *     Note:
+ *     You may assume all input has valid answer.
+ * 
+ *     Follow Up:
+ *     Can you do it in O(n) time and/or in-place with O(1) extra space?
+ * 
+ * Credits:Special thanks to @dietpepsi for adding this problem and creating all test 
+ * cases.
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
