a9ee301cbc4479aac725c1eff8c8a33ec46d46a6
yijingbai <everatnight@gmail.com>
2016-01-15 01:36:11 -0600

    Finished Bitwise and of numbers range

--- Bit/BitwiseAndOfNumbersRange/BitwiseAndOfNumbersRange.cpp
+++ Bit/BitwiseAndOfNumbersRange/BitwiseAndOfNumbersRange.cpp
@@ -2,19 +2,25 @@
 // Author : Yijing Bai
 // Date   : 2016-01-15
 
-/********************************************************************************** 
- * 
- * Given a range [m, n] where 0 
- * 
+/**********************************************************************************
+ *
+ * Given a range [m, n] where 0
+ *
  * For example, given the range [5, 7], you should return 4.
- * 
- * Credits:Special thanks to @amrsaqr for adding this problem and creating all test 
+ *
+ * Credits:Special thanks to @amrsaqr for adding this problem and creating all test
  * cases.
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        return (n > m) ? (rangeBitwiseAnd(m >> 1, n >> 1) << 1) : m;
+    }
+};
 
--- Bit/BitwiseAndOfNumbersRange/README.md
+++ Bit/BitwiseAndOfNumbersRange/README.md
@@ -1,4 +1,8 @@
 #[BitwiseAndOfNumbersRange](https://leetcode.com/problems/bitwise-and-of-numbers-range/)
-######No: ``
-######Tag: ``
+######No: `201`
+######Tag: `BitManipulation`
 ######Difficulty: `Medium`
+
+The key idea is that for the right most bit, if n > m, then there will be at least one 1 and one 0.
+And the bit And will be 0. Therefore, we just need to solve this problem and recursively solve the
+rest part of the bits by shifting them to right and them shift back.
