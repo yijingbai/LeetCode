75d3a76ce7c6a7a93f0d3cf8a49cf50078cc1ace
yijingbai <everatnight@gmail.com>
2016-01-15 17:55:33 -0600

    Fnsihed SingleNumberII

--- Bit/SingleNumberII/SingleNumberII.cpp
+++ Bit/SingleNumberII/SingleNumberII.cpp
@@ -2,23 +2,28 @@
 // Author : Yijing Bai
 // Date   : 2016-01-15
 
-/********************************************************************************** 
- * 
- * Given an array of integers, every element appears three times except for one. Find 
+/**********************************************************************************
+ *
+ * Given an array of integers, every element appears three times except for one. Find
  * that single one.
- * 
+ *
  * Note:
- * Your algorithm should have a linear runtime complexity. Could you implement it 
+ * Your algorithm should have a linear runtime complexity. Could you implement it
  * without using extra memory?
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
+    }
+};
 
