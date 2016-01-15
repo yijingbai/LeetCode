74f49b2e85de70afd735590a9c3a6e7352d8e34b
yijingbai <everatnight@gmail.com>
2016-01-14 18:39:03 -0600

    Finished Permutation Sequence

--- BackTracking/PermutationSequence/PermutationSequence.cpp
+++ BackTracking/PermutationSequence/PermutationSequence.cpp
@@ -2,31 +2,59 @@
 // Author : Yijing Bai
 // Date   : 2016-01-14
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * The set [1,2,3,…,n] contains a total of n! unique permutations.
- * 
+ *
  * By listing and labeling all of the permutations in order,
  * We get the following sequence (ie, for n = 3):
- * 
+ *
  * "123"
  * "132"
  * "213"
  * "231"
  * "312"
  * "321"
- * 
+ *
  * Given n and k, return the kth permutation sequence.
- * 
+ *
  * Note: Given n will be between 1 and 9 inclusive.
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
+#include <algorithm>
+#include <string>
+#include <cmath>
 
+using namespace std;
 
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int group_num = 1;
+        for (int i = n - 1; i >= 1; --i) {
+            group_num *= i;
+        }
 
+        int s = ceil((double)k / group_num);
+        int m = k % group_num;
+        m == 0 ? m = group_num : 0;
+        string result = to_string(s);
+
+        for (int i = 1; i <= n; ++i) {
+            if (i != s) {
+                result += to_string(i);
+            }
+        }
+
+        for (int i = 1; i < m; ++i) {
+            next_permutation(result.begin(), result.end());
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution s;
+    s.getPermutation(3, 2);
+}
--- BackTracking/PermutationSequence/README.md
+++ BackTracking/PermutationSequence/README.md
@@ -1,4 +1,25 @@
 #[PermutationSequence](https://leetcode.com/problems/permutation-sequence/)
-######No: ``
-######Tag: ``
+######No: `60`
+######Tag: `Backtracking Math`
 ######Difficulty: `Medium`
+This is a math problem. The problem is that the number of permutations is a lot.
+If we just traverse every possibile permutation, it is not possible. But Actually
+we do not need to do this. for n = 3, all possible permutations are:
+```
+123
+132
+---
+213
+231
+---
+312
+321
+---
+```
+Clearly, we could see that for each number from 1 to n, there will be (n-1)! permutatons.
+For given k, we just divide it by (n-1)!, then we know which group it belongs to. Then
+we calculate k % (n-1)!, we know which permutation is k's. Then we could get the answer.
+
+Note:
+When k % (n-1)! == 0, actually it is the last element of the group, not 0, so we need to assign
+k / (n-1)! to it.
