1958d1500f65ee6716ba93201e08355e02ebc5f9
yijingbai <everatnight@gmail.com>
2016-01-16 11:05:39 -0600

    Finished FirstMssing Positive

--- Array/FirstMissingPositive/FirstMissingPositive.cpp
+++ Array/FirstMissingPositive/FirstMissingPositive.cpp
@@ -2,24 +2,33 @@
 // Author : Yijing Bai
 // Date   : 2016-01-16
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given an unsorted integer array, find the first missing positive integer.
- * 
+ *
  * For example,
  * Given [1,2,0] return 3,
  * and [3,4,-1,1] return 2.
- * 
+ *
  * Your algorithm should run in O(n) time and uses constant space.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
 
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            while (nums[i] > 0 && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
+        }
 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.size() + 1;
+    }
+};
 
--- Array/FirstMissingPositive/README.md
+++ Array/FirstMissingPositive/README.md
@@ -1,4 +1,20 @@
 #[FirstMissingPositive](https://leetcode.com/problems/first-missing-positive/)
-######No: ``
-######Tag: ``
+######No: `41`
+######Tag: `Array`
 ######Difficulty: `Medium`
+
+Cited from https://leetcode.com/discuss/24013/my-short-c-solution-o-1-space-and-o-n-time
+```
+Put each number in its right place.
+
+For example:
+
+When we find 5, then swap it with A[4].
+
+At last, the first place where its number is not right, return the place + 1.
+```
+
+About how to put each number in its right place also need to carelly write the code.
+The idea is that we the current position's element is not equal to what it should be, swap it
+with the position where it should be. When keep doing this, finally, all the numbers should in
+its right place
