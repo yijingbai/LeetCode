eb59503a9883c875ec048662affd27b4941bd5fc
yijingbai <everatnight@gmail.com>
2016-01-15 20:48:18 -0600

    Added Implement Trie

---
+++ Design/ImplementTriePrefixTree/ImplementTriePrefixTree.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/implement-trie-prefix-tree/
+// Author : Yijing Bai
+// Date   : 2016-01-15
+
+/********************************************************************************** 
+ * 
+ * Implement a trie with insert, search, and startsWith methods.
+ * 
+ * Note:
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ Design/ImplementTriePrefixTree/README.md
@@ -0,0 +1,4 @@
+#[ImplementTriePrefixTree](https://leetcode.com/problems/implement-trie-prefix-tree/)
+######No: ``
+######Tag: ``
+######Difficulty: `Medium`
