ad5bac9a76c1c9c35f5bd075912ace0840c27647
yijingbai <everatnight@gmail.com>
2016-01-17 14:34:25 -0600

    Added Second solution for insert interval

--- Array/InsertInterval/InsertInterval.cpp
+++ Array/InsertInterval/InsertInterval.cpp
@@ -65,11 +65,24 @@ public:
         return res;
 
     }
-
-
-
-
-
-
-
 };
+
+vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+    vector<Interval> ret;
+    auto it = intervals.begin();
+    for(; it!=intervals.end(); ++it){
+        if(newInterval.end < (*it).start) //all intervals after will not overlap with the newInterval
+            break;
+        else if(newInterval.start > (*it).end) //*it will not overlap with the newInterval
+            ret.push_back(*it);
+        else{ //update newInterval bacause *it overlap with the newInterval
+            newInterval.start = min(newInterval.start, (*it).start);
+            newInterval.end = max(newInterval.end, (*it).end);
+        }
+    }
+    // don't forget the rest of the intervals and the newInterval
+    ret.push_back(newInterval);
+    for(; it!=intervals.end(); ++it)
+        ret.push_back(*it);
+    return ret;
+}
