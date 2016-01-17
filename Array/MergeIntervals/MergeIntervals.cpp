90623d4f26c4ae59d20c76318d5eb2da0bda7997
yijingbai <everatnight@gmail.com>
2016-01-17 15:36:07 -0600

    Finished Merge intervals

--- Array/MergeIntervals/MergeIntervals.cpp
+++ Array/MergeIntervals/MergeIntervals.cpp
@@ -2,22 +2,47 @@
 // Author : Yijing Bai
 // Date   : 2016-01-17
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a collection of intervals, merge all overlapping intervals.
- * 
+ *
  * For example,
  * Given [1,3],[2,6],[8,10],[15,18],
  * return [1,6],[8,10],[15,18].
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res;
+        if (intervals.empty()) return res;
+
+        sort(intervals.begin(), intervals.end(),
+                [](const Interval& l, const Interval& r){return l.start < r.start;});
+
+        int min_v = intervals[0].start, max_v = intervals[0].end;
+        for (Interval i : intervals) {
+            if (i.start <= max_v) {
+                max_v = max(i.end, max_v);
+            } else {
+                res.push_back(Interval(min_v, max_v));
+                min_v = i.start;
+                max_v = i.end;
+            }
+        }
+        res.push_back(Interval(min_v, max_v));
+        return res;
+    }
+};
 
--- Array/MergeIntervals/README.md
+++ Array/MergeIntervals/README.md
@@ -1,4 +1,7 @@
 #[MergeIntervals](https://leetcode.com/problems/merge-intervals/)
-######No: ``
+######No: `56`
 ######Tag: `Array`
 ######Difficulty: `Hard`
+Although it is a hard question, I think it is pretty easy. Just sort the overlap by its start point
+and use two pointer to mark start and end. If the interval is overlaped, the start and end pointer
+will move to the new corresponding position.
