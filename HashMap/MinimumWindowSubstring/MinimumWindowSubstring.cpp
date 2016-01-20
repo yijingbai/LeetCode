e76a0a03b8293be2dabb171ca88db6f6e78b27aa
yijingbai <everatnight@gmail.com>
2016-01-20 10:14:57 -0600

    Added minimum window substring

---
+++ HashMap/MinimumWindowSubstring/MinimumWindowSubstring.cpp
@@ -0,0 +1,33 @@
+// Source : https://leetcode.com/problems/minimum-window-substring/
+// Author : Yijing Bai
+// Date   : 2016-01-20
+
+/********************************************************************************** 
+ * 
+ * Given a string S and a string T, find the minimum window in S which will contain all 
+ * the characters in T in complexity O(n).
+ * 
+ * For example,
+ * S = "ADOBECODEBANC"
+ * T = "ABC"
+ * 
+ * Minimum window is "BANC".
+ * 
+ * Note:
+ * If there is no such window in S that covers all characters in T, return the empty 
+ * string "".
+ * 
+ * If there are multiple such windows, you are guaranteed that there will always be 
+ * only one unique minimum window in S.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ HashMap/MinimumWindowSubstring/README.md
@@ -0,0 +1,4 @@
+#[MinimumWindowSubstring](https://leetcode.com/problems/minimum-window-substring/)
+######No: ``
+######Tag: `HashMap`
+######Difficulty: `Hard`
