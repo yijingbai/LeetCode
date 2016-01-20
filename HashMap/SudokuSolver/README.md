8e461333a03bedea2670f172b05c2491aed91e45
yijingbai <everatnight@gmail.com>
2016-01-20 10:13:14 -0600

    Added sudoku solver

---
+++ HashMap/SudokuSolver/README.md
@@ -0,0 +1,4 @@
+#[SudokuSolver](https://leetcode.com/problems/sudoku-solver/)
+######No: ``
+######Tag: `HashMap`
+######Difficulty: `Hard`
---
+++ HashMap/SudokuSolver/SudokuSolver.cpp
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/sudoku-solver/
+// Author : Yijing Bai
+// Date   : 2016-01-20
+
+/********************************************************************************** 
+ * 
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * 
+ * Empty cells are indicated by the character '.'.
+ * 
+ * You may assume that there will be only one unique solution.
+ * 
+ * A sudoku puzzle...
+ * 
+ * ...and its solution numbers marked in red.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
