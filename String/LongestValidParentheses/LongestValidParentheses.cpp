f881735b9c4b9b664413f6fd761a0eb644587af5
yijingbai <everatnight@gmail.com>
2016-01-30 11:49:04 -0600

    Finished Longest Valid parentheses

--- String/LongestValidParentheses/LongestValidParentheses.cpp
+++ String/LongestValidParentheses/LongestValidParentheses.cpp
@@ -2,24 +2,34 @@
 // Author : Yijing Bai
 // Date   : 2016-01-30
 
-/********************************************************************************** 
- * 
- * Given a string containing just the characters '(' and ')', find the length of the 
+/**********************************************************************************
+ *
+ * Given a string containing just the characters '(' and ')', find the length of the
  * longest valid (well-formed) parentheses substring.
- * 
+ *
  * For "(()", the longest valid parentheses substring is "()", which has length = 2.
- * 
- * Another example is ")()())", where the longest valid parentheses substring is 
+ *
+ * Another example is ")()())", where the longest valid parentheses substring is
  * "()()", which has length = 4.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
-
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxlength = 0;
+        for (int i = 0; i < s.size(); ++i)  {
+            int t = stk.top();
+            if (t != -1 && s[i] == ')' && s[t] == '(') {
+                stk.pop();
+                maxlength = max(maxlength, i - stk.top());
+            } else {
+                stk.push(i);
+            }
+        }
+        return maxlength;
+    }
+};
--- String/LongestValidParentheses/README.md
+++ String/LongestValidParentheses/README.md
@@ -1,4 +1,15 @@
 #[LongestValidParentheses](https://leetcode.com/problems/longest-valid-parentheses/)
-######No: ``
+######No: `32`
 ######Tag: `String`
 ######Difficulty: `Hard`
+
+The idea for this problem is simple:
+cited from https://leetcode.com/discuss/7609/my-o-n-solution-using-a-stack
+```
+Scan the string from beginning to end.
+If current character is '(', push its index to the stack. If current character is ')' and the character at the index of the top of stack is '(', we just find a matching pair so pop from the stack. Otherwise, we push the index of ')' to the stack.
+After the scan is done, the stack will only contain the indices of characters which cannot be matched. Then let's use the opposite side - substring between adjacent indices should be valid parentheses.
+If the stack is empty, the whole input string is valid. Otherwise, we can scan the stack to get longest valid substring as described in step 3.
+```
+
+The key is that we can use the index to push into the stack so we need not to use other addtional variables.
