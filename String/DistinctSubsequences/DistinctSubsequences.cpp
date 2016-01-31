5bacc3293d343a317e136b49f3382b3be2dd49f9
yijingbai <everatnight@gmail.com>
2016-01-30 20:53:19 -0600

    Finished DistinctSubsequence

--- String/DistinctSubsequences/DistinctSubsequences.cpp
+++ String/DistinctSubsequences/DistinctSubsequences.cpp
@@ -2,25 +2,38 @@
 // Author : Yijing Bai
 // Date   : 2016-01-30
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Given a string S and a string T, count the number of distinct subsequences of T in S.
- * 
- * A subsequence of a string is a new string which is formed from the original string 
- * by deleting some (can be none) of the characters without disturbing the relative 
- * positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while 
+ *
+ * A subsequence of a string is a new string which is formed from the original string
+ * by deleting some (can be none) of the characters without disturbing the relative
+ * positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while
  * "AEC" is not).
- * 
+ *
  * Here is an example:
  * S = "rabbbit", T = "rabbit"
- * 
+ *
  * Return 3.
- * 
- *               
- *               
- * 
- *               
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    int numDistinct(string S, string T) {
+        int m = T.length();
+        int n = S.length();
+        if (m > n) return 0;
+        vector<vector<int>> path(m+1, vector<int>(n+1, 0));
+        for (int k = 0; k <= n; k++) path[0][k] = 1;
 
+        for (int j = 1; j <= n; j++) {
+            for (int i = 1; i <= m; i++) {
+                path[i][j] = path[i][j-1] + (T[i-1] == S[j-1] ? path[i-1][j-1] : 0);
+            }
+        }
+        return path[m][n];
+    }
+};
--- String/DistinctSubsequences/README.md
+++ String/DistinctSubsequences/README.md
@@ -1,4 +1,37 @@
 #[DistinctSubsequences](https://leetcode.com/problems/distinct-subsequences/)
-######No: ``
+######No: `115`
 ######Tag: `String`
 ######Difficulty: `Hard`
+
+We can solve this by DP in O(n^2) time.
+Cited from https://leetcode.com/problems/distinct-subsequences/
+```
+/**
+ * Solution (DP):
+ * We keep a m*n matrix and scanning through string S, while
+ * m = T.length() + 1 and n = S.length() + 1
+ * and each cell in matrix Path[i][j] means the number of distinct subsequences of 
+ * T.substr(1...i) in S(1...j)
+ * 
+ * Path[i][j] = Path[i][j-1]            (discard S[j])
+ *              +     Path[i-1][j-1]    (S[j] == T[i] and we are going to use S[j])
+ *                 or 0                 (S[j] != T[i] so we could not use S[j])
+ * while Path[0][j] = 1 and Path[i][0] = 0.
+ */
+int numDistinct(string S, string T) {
+    int m = T.length();
+    int n = S.length();
+    if (m > n) return 0;    // impossible for subsequence
+    vector<vector<int>> path(m+1, vector<int>(n+1, 0));
+    for (int k = 0; k <= n; k++) path[0][k] = 1;    // initialization
+
+    for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= m; i++) {
+            path[i][j] = path[i][j-1] + (T[i-1] == S[j-1] ? path[i-1][j-1] : 0);
+        }
+    }
+
+    return path[m][n];
+}
+
+```
