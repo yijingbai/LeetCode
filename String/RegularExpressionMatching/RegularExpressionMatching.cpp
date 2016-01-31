db9f073529e766cd634512352508b259539b1baa
yijingbai <everatnight@gmail.com>
2016-01-31 13:27:42 -0600

    FInished RegualrExpressionMatching

--- String/RegularExpressionMatching/RegularExpressionMatching.cpp
+++ String/RegularExpressionMatching/RegularExpressionMatching.cpp
@@ -2,18 +2,18 @@
 // Author : Yijing Bai
 // Date   : 2016-01-31
 
-/********************************************************************************** 
- * 
+/**********************************************************************************
+ *
  * Implement regular expression matching with support for '.' and '*'.
- * 
+ *
  * '.' Matches any single character.
  * '*' Matches zero or more of the preceding element.
- * 
+ *
  * The matching should cover the entire input string (not partial).
- * 
+ *
  * The function prototype should be:
  * bool isMatch(const char *s, const char *p)
- * 
+ *
  * Some examples:
  * isMatch("aa","a") → false
  * isMatch("aa","aa") → true
@@ -22,15 +22,20 @@
  * isMatch("aa", ".*") → true
  * isMatch("ab", ".*") → true
  * isMatch("aab", "c*a*b") → true
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
 
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
+};
