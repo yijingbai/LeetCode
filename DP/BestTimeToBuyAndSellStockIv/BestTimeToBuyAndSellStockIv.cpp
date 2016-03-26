435f8704983ed8d21e0f5f3580d8d001d40f85ee
yijingbai <everatnight@gmail.com>
2016-03-25 19:00:39 -0500

    Added best time to buy and sell stock

---
+++ DP/BestTimeToBuyAndSellStockIv/BestTimeToBuyAndSellStockIv.cpp
@@ -0,0 +1,29 @@
+// Source : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+// Author : Yijing Bai
+// Date   : 2016-03-25
+
+/********************************************************************************** 
+ * 
+ * Say you have an array for which the ith element is the price of a given stock on day 
+ * i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete at most k 
+ * transactions.
+ * 
+ * Note:
+ * You may not engage in multiple transactions at the same time (ie, you must sell the 
+ * stock before you buy again).
+ * 
+ * Credits:Special thanks to @Freezen for adding this problem and creating all test 
+ * cases.
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ DP/BestTimeToBuyAndSellStockIv/README.md
@@ -0,0 +1,4 @@
+#[BestTimeToBuyAndSellStockIv](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/)
+######No: ``
+######Tag: `DP`
+######Difficulty: `Hard`
