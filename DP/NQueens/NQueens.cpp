36754ced6d50d52df8cd112178ff597f4c8399e0
yijingbai <everatnight@gmail.com>
2016-04-01 09:07:57 -0500

    finished NQueesn

--- DP/NQueens/NQueens.cpp
+++ DP/NQueens/NQueens.cpp
@@ -2,39 +2,65 @@
 // Author : Yijing Bai
 // Date   : 2016-03-30
 
-/********************************************************************************** 
- * 
- * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such 
+/**********************************************************************************
+ *
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such
  * that no two queens attack each other.
- * 
+ *
  * Given an integer n, return all distinct solutions to the n-queens puzzle.
- * 
- * Each solution contains a distinct board configuration of the n-queens' placement, 
+ *
+ * Each solution contains a distinct board configuration of the n-queens' placement,
  * where 'Q' and '.' both indicate a queen and an empty space respectively.
- * 
+ *
  * For example,
  * There exist two distinct solutions to the 4-queens puzzle:
- * 
+ *
  * [
  *  [".Q..",  // Solution 1
  *   "...Q",
  *   "Q...",
  *   "..Q."],
- * 
+ *
  *  ["..Q.",  // Solution 2
  *   "Q...",
  *   "...Q",
  *   ".Q.."]
  * ]
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> nQueens(n, string(n, '.'));
 
+        vector<int> flag(5 * n - 2, 1);
+        solveNQueens(res, nQueens, flag, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(vector<vector<string>> &res, vector<string> &nQueens, vector<int>& flag, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col) {
+            if (flag[col] && flag[n + row + col] && flag[4 * n - 2 + col - row]) {
+                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 0;
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, flag, row + 1, n);
+                nQueens[row][col] = '.';
+                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 1;
+            }
+        }
+    }
+};
