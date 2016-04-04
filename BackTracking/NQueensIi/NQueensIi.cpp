66b4a060fba0656256f24c133cd727c74c51e569
yijingbai <everatnight@gmail.com>
2016-04-04 18:12:38 -0500

    Added NQuees

---
+++ BackTracking/NQueensIi/NQueensIi.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/n-queens-ii/
+// Author : Yijing Bai
+// Date   : 2016-04-04
+
+/********************************************************************************** 
+ * 
+ * Follow up for N-Queens problem.
+ * 
+ * Now, instead outputting board configurations, return the total number of distinct 
+ * solutions.
+ * 
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ BackTracking/NQueensIi/README.md
@@ -0,0 +1,4 @@
+#[NQueensIi](https://leetcode.com/problems/n-queens-ii/)
+######No: ``
+######Tag: `DP`
+######Difficulty: `Hard`
