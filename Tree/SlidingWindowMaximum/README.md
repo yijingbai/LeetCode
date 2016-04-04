82bd80abc4d613836d3137ffc4ed844a7dde97de
yijingbai <everatnight@gmail.com>
2016-04-04 10:24:17 -0500

    finishied sliding window max

--- Tree/SlidingWindowMaximum/README.md
+++ Tree/SlidingWindowMaximum/README.md
@@ -2,3 +2,6 @@
 ######No: ``
 ######Tag: `Tree`
 ######Difficulty: `Hard`
+
+Please Refer to the monotonic queue
+http://abitofcs.blogspot.com/2014/11/data-structure-sliding-window-minimum.html
--- Tree/SlidingWindowMaximum/SlidingWindowMaximum.cpp
+++ Tree/SlidingWindowMaximum/SlidingWindowMaximum.cpp
@@ -2,15 +2,15 @@
 // Author : Yijing Bai
 // Date   : 2016-04-04
 
-/********************************************************************************** 
- * 
- * Given an array nums, there is a sliding window of size k which is moving from the 
- * very left of the array to the very right. You can only see the k numbers in the 
+/**********************************************************************************
+ *
+ * Given an array nums, there is a sliding window of size k which is moving from the
+ * very left of the array to the very right. You can only see the k numbers in the
  * window. Each time the sliding window moves right by one position.
- * 
+ *
  * For example,
  * Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
- * 
+ *
  * ---------------               -----
  * [1  3  -1] -3  5  3  6  7       3
  *  1 [3  -1  -3] 5  3  6  7       3
@@ -18,28 +18,42 @@
  *  1  3  -1 [-3  5  3] 6  7       5
  *  1  3  -1  -3 [5  3  6] 7       6
  *  1  3  -1  -3  5 [3  6  7]      7
- * 
+ *
  * Therefore, return the max sliding window as [3,3,5,5,6,7].
- * 
- * Note: 
+ *
+ * Note:
  * You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
- * 
+ *
  * Follow up:
  * Could you solve it in linear time?
- * 
+ *
  *   How about using a data structure such as deque (double-ended queue)?
  *   The queue size need not be the same as the window’s size.
- *   Remove redundant elements and the queue should store only elements that need to be 
+ *   Remove redundant elements and the queue should store only elements that need to be
  * considered.
- * 
- *               
- *               
- * 
- *               
- *               
- * 
- *               
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **********************************************************************************/
 
 
-
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!dq.empty() && dq.front() == i-k)  dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] < nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
