b2677c2c64687d2a0a89b9492ff5e1191a8d5014
yijingbai <everatnight@gmail.com>
2016-04-04 17:10:25 -0500

    Binary tree postorder traversal

---
+++ Tree/BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/binary-tree-postorder-traversal/
+// Author : Yijing Bai
+// Date   : 2016-04-04
+
+/********************************************************************************** 
+ * 
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * 
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * 
+ * return [3,2,1].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *               
+ *               
+ * 
+ *               
+ *               
+ * 
+ *               
+ **********************************************************************************/
+
+
+
---
+++ Tree/BinaryTreePostorderTraversal/README.md
@@ -0,0 +1,4 @@
+#[BinaryTreePostorderTraversal](https://leetcode.com/problems/binary-tree-postorder-traversal/)
+######No: ``
+######Tag: `Tree`
+######Difficulty: `Hard`
